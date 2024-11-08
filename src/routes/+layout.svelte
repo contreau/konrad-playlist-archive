d68da39da61a266941f6e1150b20962a4deeff01
contreau <72581161+contreau@users.noreply.github.com>
2024-11-08 00:49:34 -0500

    kicked off building UI

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,95 @@
+<script>
+	let { children } = $props();
+</script>
+
+<header>
+	<img src="/konrad-pfp--180x180.png" alt="Konrad" height="180" width="180" />
+	<div>
+		<h1>Konrad's Songbook</h1>
+		<p>
+			<span>826</span> Original Playlists • <span>59,903</span> Songs •
+			<a href="https://open.spotify.com/user/1261690341?si=235dbc5649f8441e" target="_blank"
+				>Spotify Profile</a
+			>
+		</p>
+	</div>
+</header>
+
+<main>
+	{@render children()}
+</main>
+
+<style>
+	:global {
+		:root {
+			--text: #ededee;
+			--background: #0e0d11;
+			--primary: #b1aec8;
+			--secondary: #3d3964;
+			--accent: #a095ff;
+			--page-border: #a095ff7e;
+		}
+
+		*,
+		*::before,
+		*::after {
+			box-sizing: border-box;
+		}
+
+		html,
+		body {
+			margin: 0;
+			padding: 0;
+		}
+
+		html {
+			min-height: 100vh;
+			border-left: solid 3px var(--page-border);
+			border-right: solid 3px var(--page-border);
+			border-radius: 2px;
+		}
+
+		body {
+			font-family: system-ui;
+			color: var(--text);
+			padding-inline: 4em;
+			background-color: var(--background);
+		}
+
+		img {
+			display: block;
+		}
+
+		a {
+			color: #c0b9ff;
+		}
+
+		header {
+			margin-top: 1.5rem;
+			display: flex;
+			align-items: center;
+			gap: 6rem;
+			max-width: fit-content;
+			margin-inline: auto;
+
+			div p {
+				font-weight: 600;
+				font-size: 1.25rem;
+				span {
+					color: var(--accent);
+				}
+			}
+		}
+
+		h1 {
+			margin-top: 0;
+			margin-bottom: 1rem;
+			font-size: 5rem;
+			font-weight: 550;
+		}
+
+		main {
+			margin-top: 4rem;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,164 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import MusicNote from '$lib/svg/MusicNote.svelte';
+	import Calendar from '$lib/svg/Calendar.svelte';
+	import playlists from '$lib/playlists.json';
+	let isOpen = $state(false); // open + close state of details element
+	const entry = playlists[150];
+
+	function formatDate(dateString: string): string {
+		const months: Record<string, string> = {
+			'01': 'January',
+			'02': 'February',
+			'03': 'March',
+			'04': 'April',
+			'05': 'May',
+			'06': 'June',
+			'07': 'July',
+			'08': 'August',
+			'09': 'September',
+			'10': 'October',
+			'11': 'November',
+			'12': 'December'
+		};
+		const dateComponents = dateString.split('-');
+		let day = dateComponents[1];
+		if (day[0] === '0') {
+			day = day[1];
+		}
+		return `${months[dateComponents[0]]} ${day}, ${dateComponents[2]}`;
+	}
+</script>
+
+<svelte:head>
+	<title>Konrad's Songbook</title>
+</svelte:head>
+
+<div class="playlist">
+	<div class="playlist--info">
+		<h3><a href={entry.url} target="__blank">{entry.name}</a></h3>
+		<div class="track-info">
+			<p>
+				<span>
+					<MusicNote />
+					{entry.trackCount} songs
+				</span>
+				<span>
+					<Calendar />
+					{formatDate(entry.dateCreated)}
+				</span>
+			</p>
+		</div>
+	</div>
+
+	{#if entry.description}
+		<p class="description"><em>{entry.description}</em></p>
+	{/if}
+
+	<details bind:open={isOpen}>
+		<summary>
+			<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+				<path
+					fill="none"
+					stroke="#ffffff"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M12 5v14m-7-7h14"
+				/>
+			</svg>
+			{#if isOpen}
+				Collapse Songs
+			{:else}
+				View Songs
+			{/if}
+		</summary>
+		<ol>
+			{#each entry.tracks as track}
+				<li>
+					<a href={track.url} target="_blank">{track.name}</a> ••
+					{#each track.artists as artist, i}
+						{#if i === track.artists.length - 1}
+							{artist}
+						{:else}
+							{artist}, &nbsp;
+						{/if}
+					{/each}
+				</li>
+			{/each}
+		</ol>
+	</details>
+</div>
+
+<style>
+	h3 {
+		font-weight: 550;
+		font-size: 2.1rem;
+		margin: 1rem 0;
+	}
+
+	.playlist {
+		max-width: 50%;
+		border: solid 2px #b1aec82b;
+		border-radius: 12px;
+		padding: 0.25em 0.8em;
+	}
+
+	.track-info {
+		max-width: fit-content;
+		gap: 0.25rem;
+
+		p {
+			font-size: 1.2rem;
+			display: flex;
+			align-items: center;
+			gap: 1rem;
+			margin-top: 0;
+			margin-bottom: 0;
+		}
+	}
+
+	.playlist--info {
+		display: block;
+		align-items: center;
+		gap: 2.5rem;
+	}
+
+	.description {
+		margin-top: 1rem;
+		margin-bottom: 1rem;
+	}
+
+	ol {
+		padding-left: 1.5em;
+	}
+
+	li + li {
+		margin-top: 1.5rem;
+	}
+
+	details {
+		margin-bottom: 1rem;
+		margin-top: 1rem;
+
+		summary {
+			list-style: none;
+			font-size: 1.15rem;
+			font-weight: 500;
+			cursor: pointer;
+			display: flex;
+			align-items: center;
+		}
+	}
+
+	details summary svg {
+		transition: transform 0.3s;
+	}
+
+	details[open] summary svg {
+		transform: rotate(45deg);
+	}
+
+	:global svg {
+		margin-right: 0.2rem;
+	}
+</style>
